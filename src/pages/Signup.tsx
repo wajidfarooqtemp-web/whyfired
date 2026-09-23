93d266652058f8ffcd4a0c7e22ef8f94947a1d5e
wajid <wajidfarooqtemp@gmail.com>
2026-09-23 12:33:48 +0530

    Imported google button from components/googlebutton

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,6 +1,7 @@
 import { useState, type FormEvent } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { supabase } from "../lib/supabase";
+import GoogleButton from "../components/GoogleButton";
 
 export default function Signup() {
   const [displayName, setDisplayName] = useState("");
